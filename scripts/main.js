6ab1cb11db4cf75c44d95a86c53bf166155c5554
chandu polisetty <chandrakanthpolisetty@gmail.com>
2021-01-27 09:39:16 -0600

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,38 @@
+/**
+ * Main.js -  Logic for the app
+ * 
+ */
+// first imports
+import getLocation from './location.js';
 
+// helper functions 
 
-    
+// event handlers
+/**
+ * 
+ */
+async function locationHandler() {
+    const locText = await getLocation();
+    document.getElementById('locationAnswer').innerHTML = locText;
+}
+function clearErrorText() {
+    document.getElementById('error-message').innerHTML = '';
+}
+// declare main method
+function main() {
+    console.log('Starting main method....')
+    //get references to html elements
+
+    const locationElement = document.getElementById('location')
+    const errorElement = document.getElementById('error-message');
+
+    // init error to empty string
+    errorElement.innerHTML = '';
+
+    locationElement.addEventListener('click', locationHandler)
+    locationElement.addEventListener('touch', locationHandler);
+
+}
+
+// this is where it begins
+window.addEventListener('load', main);
